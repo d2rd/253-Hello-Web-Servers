f343094e91ec93fbeb6de7c050b2dbde2b2ededf
Glenn-David Daniel <D2rd@outlook.com>
2017-05-12 13:01:05 -0700

    Implements newEditor with listener

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,9 +1,6 @@
-// //Punit's original
-// var markdownContent = document.getElementById('flex-markdown').innerHTML
-// document.getElementById('flex-preview').innerHTML =
-//      marked(markdownContent);
-
-// //this version substitutes 'flex-markdown-input' for 'flex-markdown'
-var markdownContent = document.getElementById('markdown-textarea').value
-document.getElementById('flex-preview').innerHTML = marked(markdownContent);
-
+document.getElementById('flex-markdown-input').addEventListener('keyup', function() {
+  var markdownContent = document.getElementById('flex-markdown-input').value || ''
+  console.log('markdownContent::', markdownContent);
+  document.getElementById('flex-preview').innerHTML =
+       marked(markdownContent);
+})
